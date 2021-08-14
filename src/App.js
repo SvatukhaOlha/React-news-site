5f898a9d528310dd72c4321f6af302d95f983668
Olha Svatukha <olha.s2018@gmail.com>
2021-08-14 20:32:10 +0300

    finished project

--- src/App.js
+++ src/App.js
@@ -1,49 +1,79 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Article from './components.js/Article';
 import { makeStyles } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
-import { Paper, Container, InputBase, IconButton,
-  Box} from '@material-ui/core';
-
+import {
+  Paper,
+  Container,
+  InputBase,
+  IconButton,
+  Box,
+  Button,
+  Chip,
+} from '@material-ui/core';
+import Pagination from '@material-ui/lab/Pagination';
 
 function App() {
   const [article, setArticle] = useState([]);
-  const [search, setSearch] = useState('');
+  let [search, setSearch] = useState('');
   const [getSearch, setGetSearch] = useState('');
+  const [page, setPage] = React.useState(2);
+
+  async function getArticle(page = 0) {
+    const API_KEY = 'dd7GquFMhFh7Q57IebxRIOGqblUaRWA8';
+    const archive_API = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&page=${page}&api-key=${API_KEY}`;
+    const resSearch = await fetch(archive_API);
+    const dataSearch = await resSearch.json();
+
+    setArticle(dataSearch.response.docs);
+    setPage(page);
+  }
+
+  // PAGINATION
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+    getArticle(newPage);
+  };
 
   useEffect(() => {
-    async function getArticle() {
-      const API_KEY = 'dd7GquFMhFh7Q57IebxRIOGqblUaRWA8';
-      const archive_API = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&api-key=${API_KEY}`
-
-      const resSearch = await fetch(archive_API);
-      const dataSearch = await resSearch.json();
-      // console.log(dataSearch);
-      setArticle(dataSearch.response.docs)
-    }
-    getArticle()
-    
-  }, [getSearch])
+    getArticle();
+  }, [getSearch]);
 
   function giveInputValue(e) {
     e.preventDefault();
-    setSearch(e.target.value)
+    setSearch(e.target.value);
   }
 
   function getSearchResult(e) {
     e.preventDefault();
-    setGetSearch(search);  
+    setGetSearch(search);
   }
-  console.log(article)
+  console.log(article);
+
+  // NAVBAR SEARCH
+  let uniqueNewsDesk = [];
+  let newArticle;
+  for (let i = 0; i < article.length; i++) {
+    if (!uniqueNewsDesk.includes(article[i].section_name))
+      uniqueNewsDesk.push(article[i].section_name);
+
+    if (uniqueNewsDesk[i] === article[i].section_name) {
+      newArticle = article.filter((el) => {
+        return el.section_name === uniqueNewsDesk[i];
+      });
+    }
+  }
 
   // STYLE
   const useStyles = makeStyles((theme) => ({
     root: {
-      padding: '2px 4px',
       display: 'flex',
       alignItems: 'center',
-      width: 400,
+      width: '100%',
+      marginBottom: 15,
+      marginTop: 15,
     },
     input: {
       marginLeft: theme.spacing(1),
@@ -52,61 +82,165 @@ function App() {
     iconButton: {
       padding: 10,
     },
-  }))
+    mainImg: {
+      height: 200,
+    },
+    center: {
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      width: '30%',
+    },
+    notFoundImg: {
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      width: '100%',
+    },
+    centerButton: {
+      display: 'flex',
+      justifyContent: 'center',
+      marginBottom: 15,
+      flexWrap: 'wrap',
+    },
+  }));
 
   const classes = useStyles();
 
-
-  return (
-  <div className="App">
-    <Container 
-    maxWidth="sm">
-      <Box 
-      p={{ xs: 2, sm: 3, md: 4 }}>
-        <Paper 
-        component="form" 
-        className={classes.root}>
-          <InputBase
-            value={search}
-            onChange={giveInputValue}
-            className={classes.input}
-            placeholder="Search"
-            inputProps={{ 'aria-label': 'Search' }}
-          />
-          <IconButton 
-          onClick={getSearchResult}
-          type="submit" 
-          className={classes.iconButton} 
-          aria-label="search">
-            <SearchIcon />
-          </IconButton>
-      </Paper>
-    </Box>
-    {article.map(function(el) {
+  let results;
+  if (article.length === 0) {
+    results = 
+    <div>
+      <img 
+      className={classes.notFoundImg} 
+      src='https://d3nuqriibqh3vw.cloudfront.net/media-vimeo/70533052.jpg'></img>
+      <div className={classes.centerButton}>
+        <Chip
+        onClick={(el) => {
+          handleChangePage(el)
+        }}
+        label='Back to site'
+        variant="outlined"
+      />
+    </div>
+  </div>
+  } else {
+    results = article.map(function (el) {
+      // TITLE
       let headline = el.headline;
-      for(let k in headline) {
-        if(k.includes('main')) headline = headline[k];
+      for (let k in headline) {
+        if (k.includes('main')) headline = headline[k];
       }
 
-      let multimedia = el.multimedia[0];
-      for(let k in multimedia) {
-        if(k.includes('url')) multimedia = multimedia[k]
-      } 
-        console.log(multimedia)
-    
+      // IMAGE
+      let multimedia = el.multimedia;
+      if (multimedia.length === 0) {
+        multimedia = 'https://libraries.ou.edu/sites/default/files/NYT.png';
+      } else {
+        multimedia = el.multimedia[0];
+        for (let k in multimedia) {
+          if (k.includes('url')) multimedia = multimedia[k];
+        }
+        multimedia = 'https://www.nytimes.com/' + multimedia;
+      }
+
+      // DATE
+      let pub_date = el.pub_date.toLocaleString();
+      const date = pub_date.slice(0, 10);
+      const time = pub_date.slice(11, -5);
+      pub_date = date + ' ' + time + ' ';
+
+      // KEYWORDS
+      let keywords = el.keywords;
+      let returnKeywords = keywords.map(function (el) {
+        for (let k in el) {
+          if (k.includes('value')) {
+            keywords = el[k];
+          }
+          if(keywords.length > 50) {
+            keywords = keywords.slice(0,15)
+          }
+        }
+        return (keywords = Array(keywords));
+      });
+      console.log(returnKeywords)
+
+      // AUTHOR
+      let byline = el.byline;
+      for (let k in byline) {
+        if (k.includes('original')) byline = byline[k];
+      }
+
       return (
         <Article
-        abstract={el.abstract}
-        headline={headline}
-        multimedia={multimedia}
+          abstract={el.abstract}
+          headline={headline}
+          multimedia={multimedia}
+          web_url={el.web_url}
+          pub_date={pub_date}
+          keywords={returnKeywords}
+          byline={byline}
+          setGetSearch={setGetSearch}
+          setSearch={setSearch}
         />
-      )
-    })}
-    </Container>
-</div>
+      );
+    });
+  }
 
-)
-
+  return (
+    <div className="App">
+      <Container maxWidth="sm">
+        <div className={classes.center}>
+          <img
+            src="https://libraries.ou.edu/sites/default/files/NYT.png"
+            className={classes.mainImg}
+          ></img>
+        </div>
+        <Container className={classes.centerButton}>
+          {uniqueNewsDesk &&
+            uniqueNewsDesk.map((el) => (
+              <Button
+                onClick={(el) => {
+                  setSearch(el.target.innerText);
+                  setGetSearch(el);
+                }}
+              >
+                {el}
+              </Button>
+            ))}
+        </Container>
+      </Container>
+      <Container maxWidth="sm">
+        <Box>
+          <Paper component="form" className={classes.root}>
+            <InputBase
+              value={search}
+              onChange={giveInputValue}
+              className={classes.input}
+              placeholder="Search"
+              inputProps={{ 'aria-label': 'Search' }}
+            />
+            <IconButton
+              onClick={getSearchResult}
+              type="submit"
+              className={classes.iconButton}
+              aria-label="search"
+            >
+              <SearchIcon />
+            </IconButton>
+          </Paper>
+        </Box>
+        <Box>
+          <Pagination
+            className={classes.centerButton}
+            component="div"
+            count={100}
+            page={page}
+            onChange={handleChangePage}
+          ></Pagination>
+          {results}
+        </Box>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
--- src/components.js/Article.js
+++ src/components.js/Article.js
@@ -1,52 +1,111 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardActionArea, Typography, CardContent,
-    Button, CardActions, CardMedia} from '@material-ui/core';
+import {
+  Card,
+  CardActionArea,
+  Typography,
+  CardContent,
+  CardActions,
+  CardMedia,
+  Link,
+  Box,
+  Chip,
+} from '@material-ui/core';
 
-export default function Article({abstract, headline, multimedia}) {
+export default function Article({
+  abstract,
+  headline,
+  multimedia,
+  web_url,
+  pub_date,
+  keywords,
+  byline,
+  setGetSearch,
+  setSearch,
+}) {
+  // STYLE
+  const useStyles = makeStyles({
+    root: {
+      maxWidth: 500,
+      marginBottom: 15,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+    devider: {
+      height: 15,
+      width: 1,
+    },
+    padding: {
+      padding: 16,
+    },
+    keywords: {
+      margin: 3,
+      cursor: 'pointer',
+    },
+    paddingH: {
+      paddingTop: 8,
+      paddingBottom: 8,
+    },
+  });
 
-    // STYLE 
-    const useStyles = makeStyles({
-        root: {
-          maxWidth: 345,
-        },
-      });
-      
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="140"
-          image={multimedia}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography 
-          gutterBottom 
-          variant="h5" 
-          component="h2">
-            {headline}
-          </Typography>
-          <Typography 
-          variant="body2" 
-          color="textSecondary" 
-          component="p">
-            {abstract}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
+  return (
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="Contemplative Reptile"
+            height="250"
+            image={multimedia}
+          />
+          <CardContent>
+            <Box align="left" component="span" display="flex">
+              {pub_date}
+              {byline}
+            </Box>
+            <Typography
+              className={classes.paddingH}
+              align="left"
+              variant="h6"
+              component="h1"
+            >
+              {headline}
+            </Typography>
+            <Typography
+              variant="body2"
+              align="left"
+              color="textSecondary"
+              component="p"
+            >
+              {abstract}
+            </Typography>
+            <Box p={1}>
+              {keywords &&
+                keywords.map((el) => (
+                  <Chip
+                    onClick={(element) => {
+                        setSearch(element.target.innerText);
+                        setGetSearch(element)
+                    }}
+                    className={classes.keywords}
+                    label={el}
+                    variant="outlined"
+                  />
+                ))}
+            </Box>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Link
+            className={classes.padding}
+            href={web_url}
+            target="_blank"
+            color="textSecondary"
+          >
+            Visit site
+          </Link>
+        </CardActions>
+      </Card>
   );
 }
